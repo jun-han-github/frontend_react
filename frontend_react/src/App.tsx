0d78e597c915c7e21ba4ee10bb3bea5da0dc1992
Junhan <junhan.3008@gmail.com>
2024-09-03 23:53:07 +0800

    feat: testing jsx

--- frontend_react/src/App.tsx
+++ frontend_react/src/App.tsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -8,26 +6,25 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="main-container">
+        <label>All Products</label>
+        <input className="search-bar" placeholder='Search for...' />
+        <ul className="list">
+          <li>
+            <label>
+              <input type="checkbox"/>
+              Item 1
+            </label>
+          </li>
+          <li>
+            <label>
+              <input type="checkbox"/>
+              Item 2
+            </label>
+          </li>
+        </ul>        
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
     </>
   )
 }
